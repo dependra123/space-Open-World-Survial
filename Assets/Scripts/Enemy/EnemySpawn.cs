36e300d457249662ab6ba39c3281fb1fecde368e
dependra123 <dependra.khatri12@gmail.com>
2022-06-03 20:32:40 -0600

    thing

---
+++ Assets/Scripts/Enemy/EnemySpawn.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public Terrain terrian;
+    
+    public NavMeshAgent agent;
+    
+    Vector3 terrianSize;
+    public GameObject Enmey;
+
+    [Header("Enemy Spawn Config")]
+    public int maxEnemy = 10;
+    public int minEnemy = 5;
+    
+  
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        terrianSize = terrian.terrainData.size;
+        SpawnEnmey();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        
+    }
+
+    void SpawnEnmey()
+    {
+          //spawn enemy in random area
+        int randEnemy = Random.Range(minEnemy, maxEnemy);
+
+        for (int i = 0; i < randEnemy; i++)
+        {
+            
+            
+            int randX = Random.Range(0, (int)terrianSize.x);
+            int randZ = Random.Range(0, (int)terrianSize.z);
+            float y = terrian.SampleHeight(new Vector3(randX, 0, randZ));
+
+            Vector3 spawnPos = new Vector3(randX, y, randZ);
+
+            Instantiate(Enmey, spawnPos, Quaternion.identity);
+        }
+    }
+}
